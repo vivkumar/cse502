efb5babb3160c20df22a4a270845f2acad0564ce
Vivek Kumar <vivekk@iiitd.ac.in>
2021-02-02 15:40:27 +0530

    update fib size

--- hclib/test/lec05/fib_sequential.cpp
+++ hclib/test/lec05/fib_sequential.cpp
@@ -1,6 +1,14 @@
 #include<stdio.h>
 #include<inttypes.h>
 #include<stdlib.h>
+#include <sys/time.h>
+
+long get_usecs (void)
+{
+   struct timeval t;
+   gettimeofday(&t,NULL);
+   return t.tv_sec*1000000+t.tv_usec;
+}
 
 uint64_t fib(uint64_t n) {
   if (n < 2) { 
@@ -15,7 +23,10 @@ uint64_t fib(uint64_t n) {
 int main(int argc, char **argv) {
   if (argc < 2) { return 1; }
   uint64_t n = strtoul(argv[1], NULL, 0);
+  long start = get_usecs();
   uint64_t result = fib(n);
-  printf("Fibonacci of %" PRIu64 " is %" PRIu64 ".\n", n, result);
+  long end = get_usecs();
+  double dur = ((double)(end-start))/1000000;
+  printf("Fibonacci of %" PRIu64 " is %" PRIu64 ". Time = %0.2f\n", n, result,dur);
   return 0; 
 }
